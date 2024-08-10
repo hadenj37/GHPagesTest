bdb45254bead7367d8d6271f4a49689bca15001c
hadenj37-TAMU <hadenj37@tamu.edu>
2024-08-10 12:33:41 -0500

    Portfolio Progress

---
+++ js/funcs.js
@@ -0,0 +1,33 @@
+/// ======= GLOBALS ======= ///
+const styleNames = ["navy-style", "grey-style", "green-style", "penguin-style"];
+
+/// ======= FUNCTIONS ======= ///
+window.onload = function(){
+    // get styleIndex from local storage if it exists
+    try {
+        var styleIndex = parseInt(localStorage.getItem("styleIndex"));
+        if (styleIndex.isNaN) {
+            styleIndex = 0;
+        }
+    } catch (err) {
+        console.error(err);
+        localStorage.setItem("styleIndex", "0");
+        var styleIndex = 0;
+    }
+
+    // Set body's class to set style
+    var bodyElement = document.getElementById("bod");
+    bodyElement.className = styleNames[styleIndex];
+}
+
+function cycleStyle() {
+    // Increment styleIndex
+    var styleIndex = parseInt(localStorage.getItem("styleIndex"));
+    styleIndex += 1;
+    styleIndex %= styleNames.length;
+    localStorage.setItem("styleIndex", styleIndex.toString());
+
+    // Set body's class to set style
+    var bodyElement = document.getElementById("bod");
+    bodyElement.className = styleNames[styleIndex];
+}
