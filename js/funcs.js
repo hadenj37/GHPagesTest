a4c55b80baace2cdb07d801a0a8c9addbd6f1123
hadenj37 <hadenj37@gmail.com>
2024-08-12 16:22:21 -0500

    styling fix 3

--- js/funcs.js
+++ js/funcs.js
@@ -6,21 +6,18 @@ window.onload = function(){
     // get styleIndex from local storage if it exists
     try {
         var styleIndex = parseInt(localStorage.getItem("styleIndex"));
-        if (styleIndex.isNaN) {
+        if (isNaN(styleIndex)) {
+            localStorage.setItem("styleIndex", "0");
             styleIndex = 0;
         }
     } catch (err) {
         console.error(err);
         localStorage.setItem("styleIndex", "0");
-        var styleIndex = 0;
+        styleIndex = 0;
     }
 
     // Set body's class to set style
     var bodyElement = document.getElementById("bod");
-    if(bodyElement.className == "undefined") {
-        styleIndex = 0;
-        localStorage.setItem("styleIndex", "0");
-    }
     bodyElement.className = styleNames[styleIndex];
 }
 
@@ -33,9 +30,5 @@ function cycleStyle() {
 
     // Set body's class to set style
     var bodyElement = document.getElementById("bod");
-    if(bodyElement.className == "undefined") {
-        styleIndex = 0;
-        localStorage.setItem("styleIndex", "0");
-    }
     bodyElement.className = styleNames[styleIndex];
 }
